efecc64b91af77c7cea9e790e6ce755cd0673e5e
SzymonW66 <szymekwajs@gmail.com>
2022-04-26 10:33:43 +0200

    Adding to github 2

--- src/app/BankApp.java
+++ src/app/BankApp.java
@@ -1,5 +1,19 @@
 package app;
 
 public class BankApp {
-    private static final String
+    private static final String APP_NAME = "Aplikacja bankowa 0.5";
+    public static void main(String[] args) {
+        System.out.println(APP_NAME);
+        AppControl appControl = new AppControl();
+        appControl.mainLoop();
+
+    }
+
+
+    //uruchomienie aplikacji
+
+
+
+
+
 }
---
+++ src/model/Administrator.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Administrator {
+private final String login = "adminf";
+private final String password = "1q2w3e4r5t";
+
+}
